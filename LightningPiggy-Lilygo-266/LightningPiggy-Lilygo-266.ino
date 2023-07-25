9bd72c1903b54506ef10bbbfdeac574bc57fe25d
Thomas Farstrike <thomasverstreken@protonmail.com>
2023-07-25 13:04:41 +0200

    Turn on on-board LED to show board powered on while on battery

--- LightningPiggy-Lilygo-266/LightningPiggy-Lilygo-266.ino
+++ LightningPiggy-Lilygo-266/LightningPiggy-Lilygo-266.ino
@@ -1,4 +1,4 @@
-// Code for the Lightning Piggy running on the TTGO LilyGo 2.66 inch ePaper hardware
+// Code for the Lightning Piggy running on the TTGO LilyGo 2.66 inch ePaper (296X152) hardware.
 // https://www.lightningpiggy.com/
 //
 // Tested with:
@@ -7,13 +7,31 @@
 // ESP32 Board Support version 2.0.6
 // Preferences -> Compiler warnings: Default
 // Tools -> Board -> ESP32 Arduino -> ESP32 Dev Module
+// Tools -> Upload Speed: 921600
 // Tools -> CPU Frequency: 240Mhz
 // Tools -> Flash Frequency: 80Mhz
 // Tools -> Flash Mode: QIO
-// Tools -> Flash Size: 4MB
+// Tools -> Flash Size: 4MB (32Mb)
 // Tools -> Partition Scheme: Default 4MB with spiffs (1.2MB APP, 1.5MB SPIFFS)
 // Tools -> Core Debug Level: Warn
 // Tools -> PSRAM: Disabled
+// Tools -> Port: /dev/ttyACM0
+//
+// Make sure the Arduino IDE has permissions to access the serial port.
+// Hint: sudo chmod -f 777 /dev/ttyACM* /dev/ttyUSB*
+//
+// On the lnbits webpage:
+// - activate the LNURLp extension
+// - click on the LNURLp extension
+// - click "NEW PAY LINK"
+// - untick "fixed amount"
+// - set minimum amount: 1
+// - set maximum amount: 100000000
+// - set currency to "satoshis"
+// - click "Advanced options"
+// - set "Comment maximum characters" to 128
+// - set "Webhook URL" to https://www.lightningpiggy.com/paymentreceived
+// - set a "Success message", like: Thanks for sending sats to my piggy
 //
 #include <ArduinoJson.h>
 
@@ -56,7 +74,13 @@ int walletBalance = 0;
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Lightning Piggy version 1.0.5 starting up");
+    Serial.println("Lightning Piggy version 1.0.6 starting up");
+
+    // turn on the green LED-IO12 on the PCB
+    // otherwise there's no indication that the board is on when it's running on battery power
+    // it will turn off when the board hibernates
+    pinMode(12, OUTPUT);
+    digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
 
     SPI.begin(EPD_SCLK, EPD_MISO, EPD_MOSI);
     display.init();
