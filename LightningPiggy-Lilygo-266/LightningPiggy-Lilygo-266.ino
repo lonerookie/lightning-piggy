a404b6f70cc3b4cbb34f6647953c97df0567755a
Thomas Farstrike <thomasverstreken@protonmail.com>
2023-08-04 09:03:34 +0200

    Move display to separate file

--- LightningPiggy-Lilygo-266/LightningPiggy-Lilygo-266.ino
+++ LightningPiggy-Lilygo-266/LightningPiggy-Lilygo-266.ino
@@ -197,33 +197,6 @@ void loop() {
 }
 
 
-int displayHeight() {
-  // lilygo 2.66: 152
-  return 104; // lilygo 2.13
-}
-
-int displayWidth() {
-  // lilygo 2.66: 296
-  return 212; // lilygo 2.13
-}
-
-void displayBorder() {
-  horizontalLine();
-  verticalLine();
-}
-
-void horizontalLine() {
-    // Line for showing end of display
-    for (int16_t x = 0; x<displayWidth(); x++) {
-      display.drawPixel(x,displayHeight()+1,0);
-    }
-}
-void verticalLine() {
-    // Line for showing end of display
-    for (int16_t y = 0; y<displayHeight(); y++) {
-      display.drawPixel(displayWidth()+1,y,0);
-    }
-}
 
 void printBalance() {
   Serial.println("Displaying balance...");
@@ -434,23 +407,4 @@ String getEndpointData(String endpointUrl) {
     }
     return reply;
   }
-}
-
-void printTextCentered(char* str) {
-    int16_t x1, y1;
-    uint16_t w, h;
-
-    display.getTextBounds(str, 0, 0, &x1, &y1, &w, &h);
-    display.setCursor(displayWidth() / 2 - w / 2,
-                    displayHeight() / 2 - h / 2 );
-    display.print(str);
-}
-
-void printTextCenteredX(String str, uint16_t yPos) {
-    int16_t x1, y1;
-    uint16_t w, h;
-
-    display.getTextBounds(str, 0, 0, &x1, &y1, &w, &h);
-    display.setCursor(displayWidth() / 2 - w / 2, yPos );
-    display.print(str);
 }
